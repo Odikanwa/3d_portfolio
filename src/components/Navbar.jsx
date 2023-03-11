03f219de6ad91a4e4e6c30ca6f55382e82cef5c6
odikanwa <odikanwamichael@gmail.com>
2023-03-11 22:20:58 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-12 h-12 object-contain"></img>
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Michael <span className="sm:block hidden">| Chimezie Odikanwa</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
