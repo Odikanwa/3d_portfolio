291dae741209e6e4097f55187ce4ab361946ef62
odikanwa <odikanwamichael@gmail.com>
2023-03-18 18:59:56 +0100

    About  and Experience Sections added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop fullstack web and <br className="sm:block hidden" />
-            mobile applications. I love 3Ds.
+            mobile applications. Learning 3Ds too!
           </p>
         </div>
       </div>
@@ -28,7 +28,7 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
+            <motion.div
               animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
