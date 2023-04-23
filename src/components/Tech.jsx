72557b37777bfc779107eb284c3dbf545b3fa63a
odikanwa <odikanwamichael@gmail.com>
2023-04-23 14:52:41 +0100

    tech section modified

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -28,6 +28,14 @@ const Tech = () => {
         moment, I lean more toward front-end development.
       </motion.p>
 
+      {/* <div className="flex flex-row flex-wrap justify-center gap-10 mt-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div> */}
+
       <motion.div
         variants={slideIn("right", "tween", 0.2, 3)}
       >
@@ -39,14 +47,6 @@ const Tech = () => {
         ))}
       </div>
       </motion.div>
-
-      {/* <div className="flex flex-row flex-wrap justify-center gap-10 mt-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
-      </div> */}
     </>
   );
 };
